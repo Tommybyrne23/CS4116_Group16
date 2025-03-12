81b600121be8d7d553043dc4f958a72d05ea86d7
tori-champagne <24481033@studentmail.ul.ie>
2025-03-12 16:32:22 +0000

    Add files via upload

---
+++ initial_table_population.sql
@@ -0,0 +1,12 @@
+SELECT * FROM Businesses;
+SELECT * FROM Users;
+SELECT * FROM Admins;
+SELECT * FROM Services;
+SELECT * FROM Subservices;
+SELECT * FROM Competitions;
+
+-- Will all be empty at the initialization of our website
+SELECT * FROM Transactions;
+SELECT * FROM Reviews;
+SELECT * FROM Participants;
+SELECT * FROM Messages;
